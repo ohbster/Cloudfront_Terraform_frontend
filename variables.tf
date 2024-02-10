c3a4de130ddcf701227304ecebfac70dcd582d54
ohbster <ohbster@protonmail.com>
2024-02-10 09:14:54 -0600

    main.tf

--- variables.tf
+++ variables.tf
@@ -1,35 +1,7 @@
-########
-#Project variables
-########
-# variable "name" {
-#   type        = string
-#   description = "Main project name"
-
-#   validation {
-#     condition     = can(regex("^[a-zA-Z][a-zA-Z0-9-]{3,20}", var.name))
-#     error_message = "Invalid name: Start with letter, only letters, numbers and '-'"
-#   }
-# }
-
-# variable "regions" {
-#   type = list(object({
-#     region = string
-#     cidr   = string
-#   }))
-#   description = "Main regions"
-
-# }
-
-# variable "region" {
-#   type        = string
-#   description = "Main vpc region"
-# }
-
-# variable "region2" {
-#   type        = string
-#   description = "Main vpc region"
-# }
-
+variable "bucket_name" {
+  type = string
+  description = "Name of website's S3 bucket"
+}
 variable "common_tags" {
   type        = map(string)
   description = "Common Tags"
